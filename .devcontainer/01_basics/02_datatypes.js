ce23bd98672f4ce0e9ab865b425034b4ee391e23
zid aslam <98381862+zidaslam@users.noreply.github.com>
2024-06-25 10:57:37 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+//alert(3+3) // (it only run on browser)we are using nodejs, not browser
+console.log(3+3);
+console.log('zaid aslam');
+let name='zaid'
+let age=18
+let isLoggedIn=false
+//number => 2 
+//string =>""
+//bigint
+//boolean => true/false
+//null => standalone value (empty)
+//undefined=>  value didn't assign
+//symbol=>unique
+console.log(typeof name);  //=> you can show data type of variable
