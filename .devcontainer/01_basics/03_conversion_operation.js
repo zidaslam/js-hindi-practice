7d33c0c867a4ab3ed2a7c190bae628480669f0ca
zid aslam <98381862+zidaslam@users.noreply.github.com>
2024-06-25 12:05:49 +0000

    datatype_summary

---
+++ .devcontainer/01_basics/03_conversion_operation.js
@@ -0,0 +1,60 @@
+// 1. Conversion
+let score=33
+console.log(typeof score);
+let value='33abc'
+console.log(typeof value);
+let valueInNumber=Number(value)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);  // NaN means Not a Number
+let isLoggIn="aslam"
+let booleanIsLoggedIn=Boolean(isLoggIn)
+console.log(booleanIsLoggedIn);
+
+//1=> true
+//0=>false
+// ""=> false
+// "aslam" => true
+
+// 2. Operation
+let str1="hello"
+let str2=" Zaid"
+let str3= str1+str2
+console.log(str3);
+console.log(1+2+"2"+2+2);
+console.log("1"+2+2+2+2);
+console.log(1+2+2+2+"2");
+console.log(+true);
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
+
+// 3. Data types summary
+
+//primitive => 7 categories => 
+    // 1. string,2. Number, 3.Boolean,4. null , 5. undefined, 6. Symbol, 7.BigInt
+
+//symbol example---
+const id = Symbol('123')
+const anotherId= Symbol('123')
+console.log(id==anotherId);
+
+//bigInt example----- represent n at the last 
+const bigNumber= 12172355151547154757n
+
+//Non primitive (Reference)
+// 1.Array , 2. objects, 3. Functions 
+// Array example----
+const heros=["shaktiman","krish","dogo"];
+// Object examples--
+let myobj={
+    name:"zaid",
+    age:22,
+
+}
+
+//Function examples----
+const myFunction = function(){
+    console.log("hello world");
+}
+console.log(typeof bigNumber);
+
