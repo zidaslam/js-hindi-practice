47b06e410582280d94a16217f37a7c94fd6481a9
zid aslam <98381862+zidaslam@users.noreply.github.com>
2024-06-25 10:37:46 +0000

    variables

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,16 @@
+let name= 'zaid';
+var surName='aslam';  // now we don't use var 
+const accountId=21213;
+let accountState;
+acccountCity="jaipur"
+
+// if we change value of variables
+name='Zaid aslam'
+surName='Khan'
+//accountId= 3232;  // can not changed
+
+console.log(accountId);
+console.log(acccountCity);
+console.log(accountState);
+console.table([name,surName,accountId,acccountCity])
+
