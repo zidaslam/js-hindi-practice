556da6e25dab1aaba3e37c21e65a9c7e3558e373
zid aslam <98381862+zidaslam@users.noreply.github.com>
2024-06-26 12:50:55 +0000

    number & maths

---
+++ .devcontainer/01_basics/05_number_math.js
@@ -0,0 +1,25 @@
+// const num=50;
+// const score=400
+// console.log(score);
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// tolocalString()
+// const hundreds =10000000;
+// console.log(hundreds.toLocaleString());
+ //Indian-value
+// console.log(hundreds.toLocaleString('en-IN'));
+//---------------------------------------------Maths-----------------------------------------------
+// onvert into positive use->.abs()
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.7));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.2));  //=> give add value
+// console.log(Math.floor(4.7));  //=> add first value
+// console.log(Math.min(3,2,45,6,5,1));// => give minimum value
+// console.log(Math.max(3,2,45,6,5,1)); // => give maximum value
+console.log(Math.random()); // =>give the value between 0-1
+console.log(Math.random()*10);
+const min = 20
+const max = 10
+console.log(Math.floor(Math.random() * (max - min +1)) + min);
