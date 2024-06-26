da1dd700d6686dc779a82c9b5c580c44eb9adc2d
zid aslam <98381862+zidaslam@users.noreply.github.com>
2024-06-26 11:43:35 +0000

    strings basic

--- .devcontainer/01_basics/04_strings.js
+++ .devcontainer/01_basics/04_strings.js
@@ -1,3 +1,24 @@
 const name='Amir'
 const age= 24 
 console.log('Hello my name is '+ name +' and my age is '+ age );
+// upper case
+//console.log(name.toUpperCase());
+// The position of character by index
+// console.log(name.charAt(2));
+// console.log(name.indexOf('r'));
+// slicing character
+const gameName= 'laravel-game-coroliya-guyyi'
+// const newString= gameName.slice(-7,9)
+// console.log(newString);
+// console.log(gameName.slice(0,7));
+//trim for remove space 
+// const newStringOne="      zaid      "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+// const url="https://hitesh.com/hitesh%20choudhary";
+// console.log(url.replace('%20','-'));
+//find any string use->includes()
+// console.log(url.includes('hitesh'));
+// console.log(url.includes('ramesh'));
+// split the one string into multiple
+console.log(gameName.split("-"));
